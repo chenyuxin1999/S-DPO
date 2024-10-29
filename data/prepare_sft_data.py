d19bc460c5e6073a2490f73e5acc07a1607205e9
chenyuxin1999 <e1143404@u.nus.edu>
2024-10-29 11:05:40 +0800

    update data interface

--- data/prepare_sft_data.py
+++ data/prepare_sft_data.py
@@ -8,70 +8,19 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    # splits = ["train", "val", "test"]
-    # cans_num = 20
-    # data_dir = "../LLaRA_data/LLaRA_ml"
-    # for split in splits:
-    #     ml = MovielensData(data_dir=data_dir, stage=split, cans_num=cans_num)
-    #     dic_lis = []
-    #     for i in tqdm(range(len(ml))):
-    #         dic = {
-    #             "historyList": ml[i]["seq_name"],
-    #             "itemList": ml[i]["cans_name"],
-    #             "trueSelection": ml[i]["correct_answer"],
-    #         }
-    #         dic_lis.append(dic)
-    #     with open(f"ml-sft-cans20-new/ml-{split}.json", "w") as f:
-    #         json.dump(dic_lis, f, indent=4)
-    
-    # splits = ["train", "val", "test"]
-    # cans_num = 20
-    # data_dir = "../LLaRA_data/steam"
-    # for split in splits:
-    #     steam = SteamData(data_dir=data_dir, stage=split, cans_num=cans_num)
-    #     dic_lis = []
-    #     for i in tqdm(range(len(steam))):
-    #         dic = {
-    #             "historyList": steam[i]["seq_name"],
-    #             "itemList": steam[i]["cans_name"],
-    #             "trueSelection": steam[i]["correct_answer"],
-    #         }
-    #         dic_lis.append(dic)
-    #     with open(f"steam-sft-cans20-new/steam-{split}.json", "w") as f:
-    #         json.dump(dic_lis, f, indent=4)
-
-
-    # splits = ["train", "val", "test"]
-    # cans_num = 20
-    # data_dir = "/storage_fast/yxchen/DPO-Rec/LLaRA_data/goodreads"
-    # for split in splits:
-    #     steam = GoodreadsData(data_dir=data_dir, stage=split, cans_num=cans_num)
-    #     dic_lis = []
-    #     for i in tqdm(range(len(steam))):
-    #         # print(steam[i])
-    #         dic = {
-    #             "historyList": steam[i]["movie_seq"],
-    #             "itemList": steam[i]["cans_name"],
-    #             "trueSelection": steam[i]["next_title"],
-    #         }
-    #         dic_lis.append(dic)
-    #     with open(f"goodread-sft-cans20/goodread-{split}.json", "w") as f:
-    #         json.dump(dic_lis, f, indent=4)
-
     splits = ["train", "val", "test"]
     cans_num = 20
-    data_dir = "/storage_fast/yxchen/DPO-Rec/LLaRA_data/lastfm"
+    data_dir = ""
     for split in splits:
-        steam = LastfmData(data_dir=data_dir, stage=split, cans_num=cans_num)
+        lastfm = LastfmData(data_dir=data_dir, stage=split, cans_num=cans_num)
         dic_lis = []
-        for i in tqdm(range(len(steam))):
-            # print(steam[i])
+        for i in tqdm(range(len(lastfm))):
             dic = {
-                "historyList": steam[i]["movie_seq"],
-                "itemList": steam[i]["cans_name"],
-                "trueSelection": steam[i]["next_title"],
+                "historyList": lastfm[i]["movie_seq"],
+                "itemList": lastfm[i]["cans_name"],
+                "trueSelection": lastfm[i]["next_title"],
             }
             dic_lis.append(dic)
-        with open(f"lastfm-sft-cans20-new/lastfm-{split}.json", "w") as f:
+        with open(f"lastfm-sft-cans20/lastfm-{split}.json", "w") as f:
             json.dump(dic_lis, f, indent=4)
 
