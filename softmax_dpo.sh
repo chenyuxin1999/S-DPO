49848d7a145d38fd3f711ee0a0069d5f5fcd20f3
jf Tan <2467957427@qq.com>
2024-11-04 16:36:00 +0800

    add run bash

---
+++ softmax_dpo.sh
@@ -0,0 +1,16 @@
+torchrun --nproc_per_node 4 --master_port=25642 softmax_dpo.py \
+            --model_name base_model_path  \
+            --resume_from_checkpoint ref_model_ckpt_path \
+            --batch_size 4 \
+            --gradient_accumulation_steps 8 \
+            --dataset lastfm \
+            --prompt_path paompt_path \
+            --learning_rate 1e-5 \
+            --eval_step 0.033 \
+            --beta 1 \
+            --neg_num 3 \
+            --num_train_epochs 3 \
+            --logging_dir log_dir \
+            --output_dir save_path \
+            --wandb_project dpo-rec-nf4 \
+            --wandb_name lastfm_sdpo > log/lastfm_sdpo.log
