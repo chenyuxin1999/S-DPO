f648aca464d8b7db3180be1a791cbfb3dc213db9
chenyuxin1999 <e1143404@u.nus.edu>
2024-11-13 14:09:12 +0800

    modify sft.sh

--- sft.sh
+++ sft.sh
@@ -1,7 +1,7 @@
 # Position the number of processes specified after the --nproc_per_node flag
 torchrun --nproc_per_node 4 --master_port=25642 sft.py \
         --model_name base_model_path  \
-        --batch_size 16 \
+        --batch_size 4 \
         --gradient_accumulation_steps 8 \
         --dataset lastfm \
         --prompt_path prompt_path \
